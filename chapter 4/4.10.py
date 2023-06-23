accb5d7df012169ffa12ae7ef06677b26b6e6bb5
IIWOLFII <vladbudyachenko@mail.ru>
2023-06-23 23:51:53 +0300

    chapter 4 did the triangle the way the dude did it, struggling to understand hanoi tower

--- chapter 4/4.10.py
+++ chapter 4/4.10.py
@@ -1,1 +1,53 @@
-#holy FUCK this problem
+#holy FUCK this hanoi problem
+def largest_number(l: list):
+    if not l:
+        return None
+
+    if len(l) <= 1:
+        return l[0]
+
+    if l[0] < largest_number(l[1:]):
+        return largest_number(l[1:])
+    else:
+        return l[0]
+
+# gh = [2,4,1,2,-5,20]
+# print(largest_number(gh))
+def tower(frm, to, mink = None):
+    global poles
+
+    if len(poles[0]) == 0 and len(poles[1]) == 0:
+        return
+
+    if not mink:
+        mink = max(poles[0])
+
+
+    hands = poles[frm].pop(0)
+
+    if len(poles[2]):
+        if poles[2][0] > hands:
+            return tower(0, 1, mink)
+
+    poles[to].insert(0,hands)
+
+    print(poles)
+
+
+
+
+
+# pole1.insert(0,'value')
+# print(pole1.pop(0))
+p1 = [1,2]
+p2 = list()
+p3 = list()
+
+poles = [p1,p2,p3]
+
+print('starting with:', poles)
+tower(0,2)
+
+
+
+
--- chapter 4/4.8.py
+++ chapter 4/4.8.py
@@ -1,11 +1,14 @@
+import math
 import turtle
+from math import cos,sin,tan
 import random as rng
 
+################### \/ \/ \/
 #blind playthrough
 def triangle(t,degree,distance = 300):
     turn = 120
     if degree > 0:
-        t.fillcolor((min(255,(40 * degree*2)), 0, int(40 * degree / 2)))
+        t.fillcolor((min(255,(40 * degree*2)), 0, min(255,int(40 * degree / 2))))
         t.begin_fill()
         for _ in range(3):
             t.right(120)
@@ -15,14 +18,84 @@ def triangle(t,degree,distance = 300):
             t.left(turn)
             t.forward(distance / 2)
         t.end_fill()
-
 #different approach, oops
+################### /\ /\ /\
+
+################### \/ \/ \/
+# got it right, nice
+def draw_a_triangle(t,distance):
+    t.begin_fill()
+    pos = []
+    for _ in range(3):
+        pos.append(t.pos())
+        t.left(120)
+        t.forward(distance)
+    t.end_fill()
+    return pos
+
+def triangle_filled(t,degree,distance = 300, pos = None):
+    if degree > 0:
+        t.fillcolor((
+            max(0, 40+ int(cos(degree*0.4)*100)),
+            max(0, 50+ int(tan(degree)*50) ),
+            max(0, 120+ int(sin(degree)*130))
+        ))
+
+        # #red = int(t.ycor()*70)
+        # red = 50
+        # green = int(5*t.xcor())
+        # blue = int(255-t.ycor())
+        #
+        # color = (red%255,green%255,blue%255)
+        #
+        # colorclipped = (
+        #     max(0,min(255,red)),
+        #     max(0, min(255, green)),
+        #     max(0, min(255, blue))
+        # )
+        #
+        # t.fillcolor(color)
+        # t.begin_fill()
+
+        # avg = int(((red + green + blue)/3)-30)%255
+        # t.pencolor(avg,avg,avg)
+
+
+        pos = draw_a_triangle(t,distance)
+        posmiddles = list()
+
+        for i in [1,2]:
+            x = (pos[0][0] + pos[i][0])/2
+            y = (pos[0][1] + pos[i][1])/2
+            posmiddles.append((x,y))
+        posmiddles.append(pos[0])
+
+        for i in posmiddles:
+            t.goto(i)
+            triangle_filled(t,degree-1,int(distance/2),pos)
+
 
 fred = turtle.Turtle()
 fred.speed(0)
 window = turtle.Screen()
 window.colormode(255)
+fred.forward(300)
+fred.right(90)
+fred.forward(480)
+fred.left(90)
+fred.pensize(2)
 
-triangle(fred,5)
+triangle_filled(fred,7,1000)
+
+# fred.left(90)
+# fred.forward(480)
+# fred.right(90)
+# fred.forward(300)
+#
+# triangle(fred,4,200)
 
 window.exitonclick()
+
+
+
+
