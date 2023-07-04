874d4270fe210fe859434ee966368c9c71b7c7ca
IIWOLFII <vladbudyachenko@mail.ru>
2023-07-05 00:33:28 +0300

    chapter 4 i think i kinda understand hanoi i'll write here becauze im quirky:
    you reduce to base case so:
    you can continue the code -> the actual action of the code happens here
    then u refer back to itself in order to do the action while "being" on the base case BUT we switch the arguments around so it does a different thing

---
+++ chapter 4/4.10/tower of hanoi.py
@@ -0,0 +1,35 @@
+# inventing from scratch so i understand this kind of
+# kind of understood better i guess, still feels like im missing something
+
+def movedisk(frm,to):
+    global poles
+
+    disk = poles[frm].pop(0)
+    print(f"Moved disk {disk} from pole {frm} to pole {to}.")
+    poles[to].insert(0,disk)
+    print(poles)
+    return poles
+
+def hanoi(frm,to,height):
+    global poles
+    other = 3 - (frm+to)
+
+    if height <= 0:
+        return
+
+    hanoi(frm, other, height - 1)
+    movedisk(frm, to)
+    hanoi(other, to, height - 1)
+
+
+
+pole1 = ['A','B','C','D']
+pole2 = []
+pole3 = []
+poles = [pole1,pole2,pole3]
+
+print('Starting poles:', poles)
+
+hanoi(0,2,len(pole1))
+
+print('End poles:', poles)
